4122b7a3c72814b0cdb24d9c5416f69f8f5b4b71
Simon Lam <simonlam6174@gmai.com>
2022-11-09 11:35:39 +0000

    initial app

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, url_for, redirect
+from pymongo import MongoClient
+
+app = Flask(__name__)
+client = MongoClient('localhost', 27017)
+db = client.variantsdb
+variants = db.variants
+
+
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    return render_template('index.html')
