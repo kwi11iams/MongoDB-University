d33eeda99ef9ef6dd163a3fab9e848eb3f7af5e8
kwi11iams <katie.williams-5@postgrad.manchester.ac.uk>
2022-11-10 15:00:08 +0000

    Dropdown options for search

--- app.py
+++ app.py
@@ -26,6 +26,14 @@ def viewdb():
 @app.route('/search', methods=('GET', 'POST'))
 def searchdb():
 
+    # Var class options
+    var_classes = mongo.db.variants.distinct("var_class")
+    # Chr options
+    chr_options = mongo.db.variants.distinct("mappings.0.seq_region_name")
+    chr_options = [int(x) for x in chr_options]
+    chr_options = sorted(chr_options)
+    chr_options = [str(x) for x in chr_options]
+
     if request.method == "POST":
         var_cons = request.form["variant_consequence_search"]
         chrom = request.form["chromosome_search"]
@@ -33,28 +41,31 @@ def searchdb():
         end = request.form["end_search"]
 
         q_dict = {}
-        if var_cons:
+        if var_cons and var_cons != "NULL":
             q_dict["var_class"] = var_cons
         else: pass
 
 
-        if chrom: 
+        if chrom and chrom != "NULL":
             q_dict["mappings.0.seq_region_name"] = chrom
         else: pass
 
         if start and end:
             q_dict["mappings.0.start"] = {"$gte": int(start)}
-            
+
             q_dict["mappings.0.end"] = {"$lte": int(end)}
         else: pass
-            
+
         print(q_dict)
         query = variants.find(q_dict)
         query = query.limit(20)
 
     else:
         query = None
-    return render_template('search.html', r=query)
+    return render_template('search.html',
+                            r=query,
+                            var_classes=var_classes,
+                            chr_options=chr_options)
 
 @app.route('/variant/<ObjectId:oid>')
 def getvar(oid):
