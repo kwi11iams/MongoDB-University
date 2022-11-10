86eafa3d3ace8790460a2eca9d76a59ce5329bbb
slam2049 <simonlam6174@gmai.com>
2022-11-10 16:13:19 +0000

    edit works fuck yeah

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect
 from pymongo import MongoClient
 from flask_pymongo import PyMongo
-from flask_wtf import FlaskForm
-from wtforms import (StringField, TextAreaField, IntegerField, BooleanField,
-                     RadioField)
-from wtforms.validators import InputRequired, Length
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/variantsdb"
@@ -50,7 +47,6 @@ def searchdb():
             q_dict["mappings.0.end"] = {"$lte": int(end)}
         else: pass
             
-        print(q_dict)
         query = variants.find(q_dict)
         query = query.limit(20)
 
@@ -58,14 +54,31 @@ def searchdb():
         query = None
     return render_template('search.html', r=query)
 
-# class variant_form(FlaskForm):
-#     def __init__():
-#         ObjectId = StringField('ID')
-#         source = StringField('source')
-#         name = StringField('rsID')
-
-@app.route('/variant/<ObjectId:oid>')
-def getvar(oid):
+@app.route('/variant/<ObjectId:oid>', methods=('POST','GET'))
+def getvar(oid):    
+    if request.method == "POST":
+        q_dict = {}
+        q_dict["source"] = request.form["source"]
+        q_dict["mappings.0.location"] = request.form["position"]
+        q_dict["mappings.0.assembly_name"] = request.form["assembly"]
+        q_dict["mappings.0.seq_region_name"] = request.form["chrom"]
+        q_dict["mappings.0.strand"] = request.form["strand"]
+        q_dict["name"] = request.form["name"]
+        q_dict["MAF"] = request.form["MAF"]
+        q_dict["ambiguity"] = request.form["ambiguity"]
+        q_dict["var_class"] = request.form["var_class"]
+        q_dict["synonyms"] = request.form["synonyms"]
+        q_dict["evidence"] = request.form["evidence"]
+        q_dict["ancestral_allele"] = request.form["a_allele"]
+        q_dict["minor_allele"] = request.form["min_allele"]
+        q_dict["consequence"] = request.form["consequence"]
+        o_id = ObjectId(f'{oid}')
+        mongo.db.variants.update_one({"_id":o_id},{"$set": q_dict})
 
     record = mongo.db.variants.find_one_or_404(oid)
     return render_template('single_variant.html', variant = record)
+
+@app.route('/edit/<ObjectId:oid>', methods=('POST','GET'))
+def editvar(oid):  
+    record = mongo.db.variants.find_one_or_404(oid)
+    return render_template('edit_variant.html', variant=record)
