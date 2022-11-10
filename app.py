49d449d7271451c64c206911b40ae285a945bf69
slam2049 <simonlam6174@gmai.com>
2022-11-10 10:34:52 +0000

    single variant page in app.py

--- app.py
+++ app.py
@@ -18,25 +18,10 @@ def add():
 @app.route('/view', methods=('GET', 'POST'))
 def viewdb():
     record = variants.find()
-<<<<<<< HEAD
-    return render_template('index.html', r = record)
-=======
     return render_template('datatable.html', r = record)
->>>>>>> 11be5ef6c886853a3de63ecbaa52bf9ac8e6a014
-
 
 @app.route('/search', methods=('GET', 'POST'))
 def searchdb():
-<<<<<<< HEAD
-    if request.method == "POST":
-        var_cons = request.form["variant_consequence_search"]
-        chrom = request.form["chromosome_search"]
-
-        # if var_cons:
-        #     query = query.find({"var_class":var_cons})
-        # if chrom:
-        #     query = query.find({"mappings.0.seq_region_name":chrom})
-=======
 
     if request.method == "POST":
         var_cons = request.form["variant_consequence_search"]
@@ -44,35 +29,21 @@ def searchdb():
         start = request.form["start_search"]
         end = request.form["end_search"]
 
->>>>>>> 11be5ef6c886853a3de63ecbaa52bf9ac8e6a014
         q_dict = {}
         if var_cons:
             q_dict["var_class"] = var_cons
-        else:
-            q_dict["var_class"] = ""
-<<<<<<< HEAD
-=======
+        else: pass
 
 
->>>>>>> 11be5ef6c886853a3de63ecbaa52bf9ac8e6a014
         if chrom: 
             q_dict["mappings.0.seq_region_name"] = chrom
-        else:
-            q_dict["mappings.0.seq_region_name"] = ""
+        else: pass
 
-<<<<<<< HEAD
-
-        query = variants.find(q_dict)
-        query = query.limit(20)
-        print(type(query))
-    else:
-        query = ["nothin to see ere"]
-    return render_template('search.html', records = query)
-=======
         if start and end:
             q_dict["mappings.0.start"] = {"$gte": int(start)}
             
             q_dict["mappings.0.end"] = {"$lte": int(end)}
+        else: pass
             
         print(q_dict)
         query = variants.find(q_dict)
@@ -81,4 +52,8 @@ def searchdb():
     else:
         query = None
     return render_template('search.html', r=query)
->>>>>>> 11be5ef6c886853a3de63ecbaa52bf9ac8e6a014
+
+@app.route('/variant/<oid>')
+def getvar(oid):
+    record = variants.find({"_id":f"ObjectID('{oid}')"})
+    return render_template(single_variant.html, variant = record)
