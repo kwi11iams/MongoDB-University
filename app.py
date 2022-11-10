977f8d958161483c23468d27d67d610d0a612736
slam2049 <simonlam6174@gmai.com>
2022-11-10 16:24:41 +0000

    now it actually works

--- app.py
+++ app.py
@@ -78,6 +78,24 @@ def searchdb():
 
 @app.route('/variant/<ObjectId:oid>')
 def getvar(oid):
+    if request.method == "POST":
+        q_dict = {}
+        q_dict["source"] = request.form["source"]
+        q_dict["mappings.0.location"] = request.form["position"]
+        q_dict["mappings.0.assembly_name"] = request.form["assembly"]
+        q_dict["mappings.0.seq_region_name"] = request.form["chrom"]
+        q_dict["mappings.0.strand"] = request.form["strand"]
+        q_dict["name"] = request.form["name"]
+        q_dict["MAF"] = request.form["MAF"]
+        q_dict["ambiguity"] = request.form["ambiguity"]
+        q_dict["var_class"] = request.form["var_class"]
+        q_dict["synonyms"] = request.form["synonyms"]
+        q_dict["evidence"] = request.form["evidence"]
+        q_dict["ancestral_allele"] = request.form["a_allele"]
+        q_dict["minor_allele"] = request.form["min_allele"]
+        q_dict["consequence"] = request.form["consequence"]
+        o_id = ObjectId(f'{oid}')
+        mongo.db.variants.update_one({"_id":o_id},{"$set": q_dict})
     record = mongo.db.variants.find_one_or_404(oid)
     return render_template('single_variant.html', variant = record)
 
